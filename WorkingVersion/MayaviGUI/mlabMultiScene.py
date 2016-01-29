c05ee537f2a71d8c3daaf1004dd1c47afe9d02a6
Cheng Zhang <czhang0328@gmail.com>
2016-01-29 15:33:03 -0500

    Tried GUI

---
+++ WorkingVersion/MayaviGUI/mlabMultiScene.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+from traits.api import HasTraits, Instance, Button, \
+    on_trait_change
+from traitsui.api import View, Item, HSplit, Group
+
+from mayavi import mlab
+from mayavi.core.ui.api import MlabSceneModel, SceneEditor
+
+
+class MyDialog(HasTraits):
+
+    scene1 = Instance(MlabSceneModel, ())
+    scene2 = Instance(MlabSceneModel, ())
+
+    button1 = Button('Redraw')
+    button2 = Button('Redraw')
+
+    @on_trait_change('button1')
+    def redraw_scene1(self):
+        self.redraw_scene(self.scene1)
+
+    @on_trait_change('button2')
+    def redraw_scene2(self):
+        self.redraw_scene(self.scene2)
+
+    def redraw_scene(self, scene):
+        # Notice how each mlab call points explicitely to the figure it
+        # applies to.
+        mlab.clf(figure=scene.mayavi_scene)
+        x, y, z, s = np.random.random((4, 100))
+        mlab.points3d(x, y, z, s, figure=scene.mayavi_scene)
+
+    # The layout of the dialog created
+    view = View(HSplit(
+                  Group(
+                       Item('scene1',
+                            editor=SceneEditor(), height=250,
+                            width=300),
+                       'button1',
+                       show_labels=False,
+                  ),
+                  Group(
+                       Item('scene2',
+                            editor=SceneEditor(), height=250,
+                            width=300, show_label=False),
+                       'button2',
+                       show_labels=False,
+                  ),
+                ),
+                resizable=True,
+                )
+
+
+m = MyDialog()
+m.configure_traits()
